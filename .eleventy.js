bb1694285892e221c5cdf0173810b458336ca90d
Sonja Vainionpää <98089784+sonyavpaa@users.noreply.github.com>
2023-08-01 11:40:35 +0300

    netlify build

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
 
 module.exports = config => {
 
-      // Set directories to pass through to the dist folder
-      config.addPassthroughCopy('./src/images/');
+        // Transforms
+        const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+
+      // Create a helpful production flag
+      const isProduction = process.env.NODE_ENV === 'production';
+
+      // Only minify HTML if we are in production because it slows builds _right_ down
+      if (isProduction) {
+        config.addTransform('htmlmin', htmlMinTransform);
+      }
 
       config.addCollection('art', collection => {
         return collection.getFilteredByGlob('./src/art/*.md').sort((a, b) => {
